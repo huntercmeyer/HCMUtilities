b4f211a19f20ffbec3ba34407ca0681f82105750
Hunter Meyer <huntercmeyer98@gmail.com>
2021-12-31 22:09:56 -0600

    Added an extension to Dictionary that will return the first key or first value (depending on the function used) if a predicate is matched.

---
+++ Sources/HCMUtilities/Dictionary+firstKey.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Hunter Meyer on 12/31/21.
+//
+
+import Foundation
+
+extension Dictionary {
+    /// Returns the key to the first element in the dictionary that satifies the given predicate, if there is one.
+    /// - Parameter predicate: A closure that takes an element of the dictionary as an argument and returns a Boolean indicating whether the element is a match.
+    /// - Returns: The key of the first dictionary element that satisfies the predicate, or nil if no elements satisfy the predicate.
+    func firstKey(where predicate: (Key, Value) -> Bool) -> Key? {
+        return self.first(where: predicate)?.key
+    }
+}
